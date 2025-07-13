3e8a3c041bbe98ce02e906d76570561a50025d8a
rayyan22207 <rayyan22207@gmail.com>
2025-07-13 22:56:23 +0500

    working on I am watching the video and copy pasting will work on edits to make it fun and more understanding as this is mostly learning more

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import Chat from "./components/chat/Chat"
-import Detail from "./components/Detail/Detail"
-import List from "./components/List/List"
+import { useEffect } from "react";
+import Chat from "./components/chat/Chat";
+import Detail from "./components/detail/Detail";
+import List from "./components/list/List";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
-  return (
-    <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
-    </div>
-  )
-}
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+  const { chatId } = useChatStore();
 
-export default App
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
+  return (
+    <div className="container">
+      {currentUser ? (
+        <>
+          <List />
+          {chatId && <Chat />}
+          {chatId && <Detail />}
+        </>
+      ) : (
+        <Login />
+      )}
+      <Notification />
+    </div>
+  );
+};
+
+export default App;
